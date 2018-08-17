e32127d304a0a2a520446ede544f3640761c13d0
bifenglin <515186469@qq.com>
2018-08-17 21:45:24 +0800

    fix request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,9 @@
 import fetch from 'dva/fetch';
+import { notification } from 'antd';
+import { routerRedux } from 'dva/router';
+import store from '../index';
 
-/* const codeMessage = {
+const codeMessage = {
   200: '服务器成功返回请求的数据。',
   201: '新建或修改数据成功。',
   202: '一个请求已经进入后台排队（异步任务）。',
@@ -12,11 +15,26 @@ import fetch from 'dva/fetch';
   406: '请求的格式不可得。',
   410: '请求的资源被永久删除，且不会再得到的。',
   422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
+  // 500: '服务器发生错误，请检查服务器。',
   502: '网关错误。',
   503: '服务不可用，服务器暂时过载或维护。',
   504: '网关超时。',
-}; */
+};
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const errortext = codeMessage[response.status] || response.message;
+  notification.error({
+    message: `请求错误 ${response.status}: ${response.url}`,
+    description: errortext,
+  });
+  const error = new Error(errortext);
+  error.name = response.status;
+  error.response = response;
+  throw error;
+}
 
 /**
  * Requests a URL, returning a promise.
@@ -50,8 +68,31 @@ export default function request(url, options) {
       };
     }
   }
-  // TO DO
+
   return fetch(url, newOptions).then(response => {
-    return response.json();
+    return response
+      .json()
+      .then(checkStatus)
+      .catch(e => {
+        const { dispatch } = store;
+        const status = e.name;
+        if (status === 401) {
+          dispatch({
+            type: 'login/logout',
+          });
+          return;
+        }
+        if (status === 403) {
+          dispatch(routerRedux.push('/exception/403'));
+          return;
+        }
+        // if (status <= 504 && status >= 500) {
+        //   dispatch(routerRedux.push('/exception/500'));
+        //   return;
+        // }
+        if (status >= 404 && status < 422) {
+          dispatch(routerRedux.push('/exception/404'));
+        }
+      });
   });
 }
