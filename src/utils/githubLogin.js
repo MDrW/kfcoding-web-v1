2e2c990963fe94abdfccfff2a9a8df1245e1f185
liwen-tj <3407617286@qq.com>
2018-08-17 21:31:52 +0800

    eslint

--- src/utils/githubLogin.js
+++ src/utils/githubLogin.js
@@ -1,22 +1,19 @@
 import { openWindow } from './openWindow';
-import { setLocalData } from './localData';
-import Settings from '../common/settings'
+// import { setLocalData } from './localData';
+import Settings from '../common/settings';
 
 export function thirdPartyLogin() {
-  openWindow(
-    Settings.GITHUB,
-    '登录',
-    600,
-    600
-  );
-  
-  window.addEventListener('message', (m) => {
-    setLocalData('token', m.data.token);
-    currentUser().then(res => {
-      console.log(res)
-        setLocalData('uid', res.data.result.user.id);
-        setLocalData('user', JSON.stringify(res.data.result.user));
-        window.location.replace('/');
-      });
-    })
+  openWindow(Settings.GITHUB, '登录', 600, 600);
+
+  // TO DO
+
+  // window.addEventListener('message', m => {
+  //   setLocalData('token', m.data.token);
+  //   currentUser().then(res => {
+  //     console.log(res);
+  //     setLocalData('uid', res.data.result.user.id);
+  //     setLocalData('user', JSON.stringify(res.data.result.user));
+  //     window.location.replace('/');
+  //   });
+  // });
 }
