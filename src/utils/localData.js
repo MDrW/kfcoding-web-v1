a1e63af4cb6779912cde443f0eb878c246cf6911
liwen-tj <3407617286@qq.com>
2018-08-17 21:33:52 +0800

    eslint

--- src/utils/localData.js
+++ src/utils/localData.js
@@ -1,18 +1,18 @@
 import Cookies from 'universal-cookie';
 
-const options = {path: '/', domain: '*kfcoding.com'};
+const options = { path: '/', domain: '*kfcoding.com' };
 
-export function setLocalData(name, value){
+export function setLocalData(name, value) {
   const cookie = new Cookies();
   cookie.set(name, value, options);
 }
 
-export function getLocalData(name){
+export function getLocalData(name) {
   const cookie = new Cookies();
   return cookie.get(name, options);
 }
 
-export function removeLocalData(name){
+export function removeLocalData(name) {
   const cookie = new Cookies();
   cookie.remove(name, options);
 }
